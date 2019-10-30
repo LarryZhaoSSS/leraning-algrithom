4339cd666866f052fc3f4e8e68b4ec44c161a26c
LarryKameZhao <larryzhao1994@outlook.com>
2019-10-31 07:46:35 +0800

    add and override toString method for my Array

---
+++ array/src/Array.java
@@ -0,0 +1,63 @@
+public class Array {
+    private int[] data;
+    private int size;
+
+    // 传入数组的容量capacity构造Array
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    // 无参数的构造函数，默认容量是10
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    // 返回数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addLast(int e) {
+        add(size,e);
+    }
+    public void addFirst(int e) {
+        add(0,e);
+    }
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("Array is full");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index > 0 && index<size");
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array:size=%d,capacity=%d\n",size,data.length));
+        res.append('[');
+        for(int i=0;i<size;i++) {
+            res.append(data[i]);
+            if(i!=size-1) {
+                res.append(", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+}
--- array/src/Main.java
+++ array/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Array arr = new Array(30);
+       for(int i=0;i<10;i++) {
+           arr.addLast(i);
+       }
+       System.out.println(arr);
+//       arr.add(1,100);
+//       System.out.println(arr);
+       arr.addFirst(-1);
+       System.out.println(arr);
     }
 }
