931be195b0fe598fbc9db3322bc9bc5cd9bc1308
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-13 00:21:48 +0800

    auto resize array

--- array/src/Main.java
+++ array/src/Main.java
@@ -1,27 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-        Array<Integer> arr = new Array(30);
+        Array<Integer> arr = new Array(10);
         for (int i = 0; i < 10; i++) {
             arr.addLast(i);
         }
         System.out.println(arr);
-//       arr.add(1,100);
-//       System.out.println(arr);
-        arr.addFirst(-1);
+        arr.addLast(100);
         System.out.println(arr);
-
-
-        arr.remove(2);
+        arr.remove(0);
+        arr.remove(0);
         System.out.println(arr);
-
-        arr.removeElement(4);
-        System.out.println(arr);
-
-        arr.removeFirst();
-        System.out.println(arr);
-
-        arr.removeLast();
-        System.out.println(arr);
+//        arr.add(1,100);
+//        arr.addFirst(-1);
+//        System.out.println(arr);
+//        arr.remove(1);
+//        arr.remove(2);
+//        arr.remove(3);
+//        System.out.println(arr);
+//        System.out.println(arr);
+////       arr.add(1,100);
+////       System.out.println(arr);
+//        arr.addFirst(-1);
+//        System.out.println(arr);
+//
+//
+//        arr.remove(2);
+//        System.out.println(arr);
+//
+//        arr.removeElement(4);
+//        System.out.println(arr);
+//
+//        arr.removeFirst();
+//        System.out.println(arr);
+//
+//        arr.removeLast();
+//        System.out.println(arr);
     }
 }
