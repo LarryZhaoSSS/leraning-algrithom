700dfeda0f08d5c1d91add54cb7ce2d4ef2825fe
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-21 07:58:03 +0800

    loop queue

---
+++ Queue/src/com/company/LoopQueue.java
@@ -0,0 +1,87 @@
+package com.company;
+
+public class LoopQueue<E> implements Queue<E> {
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    public LoopQueue(int capacity) {
+        //循环队列浪费1个空间，所以期望的值+1
+        data = (E[]) new Object[capacity + 1];
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() * 2);
+        }
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty())
+            throw new IllegalArgumentException("cannot dequeue fron an empty queue");
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("queue is Empty");
+        }
+        return data[front];
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue:size=%d,capacity=%d\n", size, getCapacity()));
+        res.append("front[");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            res.append(data[i]);
+            if ((i + 1) % data.length != tail) {
+                res.append(", ");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+}
--- Queue/src/com/company/Main.java
+++ Queue/src/com/company/Main.java
@@ -3,17 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    ArrayQueue<Integer> queue = new ArrayQueue<Integer>();
+//	    ArrayQueue<Integer> queue = new ArrayQueue<Integer>();
+//	    for(int i = 0;i<10;i++) {
+//	        queue.enqueue(i);
+//	        System.out.println(queue);
+//        }
+//	    int queueSize = queue.getSize();
+//		for(int j=0;j<queueSize-1;j++) {
+//			if(j %2 ==0) {
+//				queue.dequeue();
+//			}
+//		}
+//		System.out.println(queue);
+		LoopQueue<Integer> queue = new LoopQueue<Integer>();
 	    for(int i = 0;i<10;i++) {
 	        queue.enqueue(i);
-//	        System.out.println(queue);
-        }
-	    int queueSize = queue.getSize();
-		for(int j=0;j<queueSize-1;j++) {
-			if(j %2 ==0) {
+			if(i %3 ==2) {
 				queue.dequeue();
 			}
-		}
-		System.out.println(queue);
+	        System.out.println(queue);
+        }
+
     }
 }
