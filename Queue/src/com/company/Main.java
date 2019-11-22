7cb2fefac2783cc63baabba9a08f408d0ce55265
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-22 08:05:55 +0800

    compare loopQue and arrayQue

--- Queue/src/com/company/Main.java
+++ Queue/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,14 +17,33 @@ public class Main {
 //			}
 //		}
 //		System.out.println(queue);
-		LoopQueue<Integer> queue = new LoopQueue<Integer>();
-	    for(int i = 0;i<10;i++) {
-	        queue.enqueue(i);
-			if(i %3 ==2) {
-				queue.dequeue();
-			}
-	        System.out.println(queue);
-        }
+//		LoopQueue<Integer> queue = new LoopQueue<Integer>();
+//	    for(int i = 0;i<10;i++) {
+//	        queue.enqueue(i);
+//			if(i %3 ==2) {
+//				queue.dequeue();
+//			}
+//	        System.out.println(queue);
+//        }
+		int opCount = 100000;
+		ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
+		double time1 = testQueue(arrayQueue,opCount);
+		System.out.println("ArrayQue time:" + time1 +"秒");
+		LoopQueue<Integer> loopQueue = new LoopQueue<Integer>();
+		double time2 = testQueue(loopQueue,opCount);
+		System.out.println("LoopQue time:" + time2 +"秒");
 
     }
+    public static double testQueue(Queue<Integer> q, int opCount) {
+    	long startTime = System.nanoTime();
+		Random random = new Random();
+		for(int i=0;i<opCount;i++) {
+			q.enqueue(random.nextInt(Integer.MAX_VALUE));
+		}
+		for(int i=0;i<opCount;i++){
+			q.dequeue();
+		}
+    	long endTime = System.nanoTime();
+    	return (endTime - startTime) / 1000.0 /1000.0 /1000.0;
+	}
 }
