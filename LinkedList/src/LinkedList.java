892eba918af031ef2cd081d636d9a49c98719110
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-26 00:03:06 +0800

    head and tail add element

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -7,15 +7,62 @@ public class LinkedList<E> {
             this.e = e;
             this.next = next;
         }
+
         public Node(E e) {
-            this(e,null);
+            this(e, null);
         }
-        public Node(){
-            this(null,null);
+
+        public Node() {
+            this(null, null);
         }
+
         @Override
         public String toString() {
             return e.toString();
         }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+        Node node = new Node(e);
+        node.next = head;
+        head = node;
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("illegal index");
+        }
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            Node node = new Node(e);
+            node.next = prev.next;
+            prev.next = node;
+            size ++;
+        }
+    }
+    public void addLast(E e) {
+        add(size,e);
     }
 }
