6bed3916c3e92735b9e09fc8e67e9c3dd6e42df7
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-26 07:49:13 +0800

    linked list

--- LinkedList/src/com/company/Main.java
+++ LinkedList/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    LinkedList<Integer> linkedList = new LinkedList<Integer>();
+	    for(int i=0;i<10;i++) {
+	        linkedList.addFirst(i);
+//			System.out.println(linkedList);
+        }
+
+	    linkedList.addLast(999);
+	    System.out.println("after insert :");
+        System.out.println(linkedList);
+
+        linkedList.add(2,666);
+		System.out.println("after insert :");
+		System.out.println(linkedList);
+
+		linkedList.set(3,333);
+		System.out.println("after update :");
+		System.out.println(linkedList);
     }
 }
