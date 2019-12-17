fa8f3b4901449598abac301d234bcf1e3d07451d
LarryKameZhao <larryzhao1994@outlook.com>
2019-12-17 23:22:21 +0800

    BST add

---
+++ RemoveElmentInListNode/src/ListNode.java
@@ -0,0 +1,30 @@
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+    public ListNode(int[] arr) {
+        if(arr==null || arr.length == 0) {
+            throw new IllegalArgumentException("arr illegal");
+        }
+        this.val = arr[0];
+        ListNode cur = this;
+        for(int i=1;i<arr.length;i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        while(cur != null) {
+            res.append(cur.val +"-->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
