f0e25eb15743f05a9de04ee1c6d4c1860a3059e1
LarryKameZhao <larryzhao1994@outlook.com>
2019-12-18 23:13:02 +0800

    BST preOrder

--- BST/src/com/company/BST.java
+++ BST/src/com/company/BST.java
@@ -33,36 +33,76 @@ public class BST<E extends Comparable<E>> {
             root = new Node(e);
             size++;
         } else {
-            add(root,e);
+            add(root, e);
         }
     }
 
     //返回插入新节点后二分搜索树的根
     private Node add(Node node, E e) {
-        if(node == null) {
+        if (node == null) {
             size++;
             return new Node(e);
         }
         if (e.compareTo(node.e) < 0) {
-           node.left = add(node.left, e);
-        } else if(e.compareTo(node.e)>0) {
-           node.right = add(node.right, e);
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
         }
         return node;
     }
+
     public boolean contains(E e) {
-        return contains(root,e);
+        return contains(root, e);
     }
+
     private boolean contains(Node node, E e) {
-        if(node == null) {
+        if (node == null) {
             return false;
         }
-        if(e.compareTo(node.e)==0) {
+        if (e.compareTo(node.e) == 0) {
             return true;
-        } else if(e.compareTo(node.e)<0) {
-            return contains(node.left,e);
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
         } else {
-            return contains(node.right,e);
+            return contains(node.right, e);
         }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
     }
 }
