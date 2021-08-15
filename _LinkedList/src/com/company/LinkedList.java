6c436d0f9b28cf3f09e7c9084f9689de1610dd03
LarryZhao <larryzhao1994@outlook.com>
2021-08-15 21:54:33 +0800

    LinkedList-delete item

--- _LinkedList/src/com/company/LinkedList.java
+++ _LinkedList/src/com/company/LinkedList.java
@@ -4,59 +4,134 @@ public class LinkedList<E> {
     private class Node {
         public E e;
         public Node next;
-        public Node(E e,Node next) {
+
+        public Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
+
         public Node(E e) {
-            this(e,null);
+            this(e, null);
         }
+
         public Node() {
-            this(null,null);
+            this(null, null);
         }
+
         @Override
         public String toString() {
             return e.toString();
         }
     }
-    private Node head;
+
+    private Node dummyHead;
     private int size;
+
     public LinkedList() {
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
+
     public int getSize() {
         return size;
     }
+
     public boolean isEmpty() {
         return size == 0;
     }
+
     public void addFirst(E e) {
 //        Node node = new Node(e);
 //        node.next = head;
 //        head = node;
-        head = new Node(e,head);
-        size++;
+        add(0,e);
     }
-    public void add(int index,E e) {
-        if(index <0 || index > size) {
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
             throw new IllegalArgumentException("invalid index");
         }
-        if(index == 0) {
-            addFirst(e);
-        } else {
-            Node prev = head;
-            for(int i=0;i<index -1;i++) {
-                prev = prev.next;
-            }
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index ; i++) {
+            prev = prev.next;
+        }
 //            Node node = new Node(e);
 //            node.next = prev.next;
 //            prev.next = node;
-            prev.next = new Node(e,prev.next);
-            size++;
-        }
+        prev.next = new Node(e, prev.next);
+        size++;
+
     }
+
     public void addLast(E e) {
-        add(size,e);
+        add(size, e);
+    }
+    public E get(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("invalid index");
+        }
+        Node cur = dummyHead.next;
+        for(int i=0;i<index;i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+    public E getFirst() {
+        return get(0);
+    }
+    public E getLast() {
+        return get(size-1);
+    }
+    public void set(int index,E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("invalid index");
+        }
+        Node cur = dummyHead.next;
+        for(int i=0;i<index;i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+    public boolean contains(E e) {
+        Node cur =dummyHead.next;
+        while(cur != null) {
+            if(cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+    public E remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("invalid index");
+        }
+        Node prev = dummyHead;
+        for(int i=0;i<index;i++) {
+            prev = prev.next;
+        }
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+        return retNode.e;
+    }
+    public E removeFirst() {
+        return remove(0);
+    }
+    public  E removeLast() {
+        return remove(size -1);
+    }
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while(cur != null) {
+            res.append(cur+"->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
     }
 }
