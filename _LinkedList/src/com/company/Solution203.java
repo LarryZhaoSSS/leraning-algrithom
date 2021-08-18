56a5910a26a6f5e588a5b0f07543254033e423f2
LarryZhao <larryzhao1994@outlook.com>
2021-08-18 21:45:06 +0800

    remove elements in LinkedList

---
+++ _LinkedList/src/com/company/Solution203.java
@@ -0,0 +1,48 @@
+package com.company;
+
+// remove  elements
+public class Solution203 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+        if(head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        while(prev.next != null) {
+            if(prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+    public ListNode removeByDummyHead(ListNode head,int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        while(prev.next != null) {
+            if(prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dummyHead.next;
+    }
+}
