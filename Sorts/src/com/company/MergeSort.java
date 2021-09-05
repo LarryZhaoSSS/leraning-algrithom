98274445dadc446707cfa04e9be6876172ca65a9
LarryZhao <larryzhao1994@outlook.com>
2021-09-05 22:04:38 +0800

    Merge sortBU

--- Sorts/src/com/company/MergeSort.java
+++ Sorts/src/com/company/MergeSort.java
@@ -3,76 +3,101 @@ package com.company;
 import java.util.Arrays;
 
 public class MergeSort {
-    private MergeSort() {}
-    public static <E extends  Comparable<E>>void sort(E[] arr) {
-        sort(arr,0, arr.length-1);
+    private MergeSort() {
     }
-    private static <E extends  Comparable<E>> void sort(E[] arr,int l,int r) {
-        if(r-l<=15) {
-            InsertionSort.sort(arr,l,r);
+
+    public static <E extends Comparable<E>> void sortBU(E[] arr) {
+        int n = arr.length;
+        // 遍历合并的区间长度
+        for (int sz = 1; sz < n; sz += sz) {
+            // 遍历合并的两个区间的起始位置i
+            //合并[i,i+sz-1]和[i+sz,Math.min(i+sz+sz-1,n-1)]
+            for (int i = 0; i+sz<n ; i += sz + sz) {
+                if(arr[i+sz-1].compareTo(arr[i+sz])>0) {
+                    merge(arr,i,i+sz-1,Math.min(i+sz+sz-1,n-1));
+                }
+
+            }
+        }
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
+        if (r - l <= 15) {
+            InsertionSort.sort(arr, l, r);
             return;
         }
-        int mid = (l+r) / 2;
-        sort(arr,l,mid);
-        sort(arr,mid+1,r);
-        merge(arr,l,mid,r);
+        int mid = (l + r) / 2;
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
 
     }
-    public static <E extends  Comparable<E>>void sort2(E[] arr) {
-        sort2(arr,0, arr.length-1);
+
+    public static <E extends Comparable<E>> void sort2(E[] arr) {
+        sort2(arr, 0, arr.length - 1);
     }
-    private static <E extends  Comparable<E>> void sort2(E[] arr,int l,int r) {
-        if(l >= r) {
+
+    private static <E extends Comparable<E>> void sort2(E[] arr, int l, int r) {
+        if (l >= r) {
             return;
         }
-        int mid = (l+r) / 2;
-        sort2(arr,l,mid);
-        sort2(arr,mid+1,r);
-        if(arr[mid].compareTo(arr[mid+1])>0) {
-            merge(arr,l,mid,r);
+        int mid = (l + r) / 2;
+        sort2(arr, l, mid);
+        sort2(arr, mid + 1, r);
+        if (arr[mid].compareTo(arr[mid + 1]) > 0) {
+            merge(arr, l, mid, r);
         }
 
 
     }
-    public static <E extends  Comparable<E>>void sort3(E[] arr) {
-        sort3(arr,0, arr.length-1);
+
+    public static <E extends Comparable<E>> void sort3(E[] arr) {
+        sort3(arr, 0, arr.length - 1);
     }
-    private static <E extends  Comparable<E>> void sort3(E[] arr,int l,int r) {
-        if(l >= r) {
+
+    private static <E extends Comparable<E>> void sort3(E[] arr, int l, int r) {
+        if (l >= r) {
             return;
         }
-        int mid = (l+r) / 2;
-        sort3(arr,l,mid);
-        sort3(arr,mid+1,r);
-        if(arr[mid].compareTo(arr[mid+1])>0) {
-            merge(arr,l,mid,r);
+        int mid = (l + r) / 2;
+        sort3(arr, l, mid);
+        sort3(arr, mid + 1, r);
+        if (arr[mid].compareTo(arr[mid + 1]) > 0) {
+            merge(arr, l, mid, r);
         }
 
 
     }
-    private static <E extends Comparable<E>> void merge(E[] arr,int l,int mid,int r) {
-        E[] temp = Arrays.copyOfRange(arr,l,r+1);
-        int i=l,j=mid+1;
-        for(int k=l;k<=r;k++) {
-            if(i>mid) {
-                arr[k] = temp[j-l];
+
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
+        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = temp[j - l];
                 j++;
-            } else if(j>r) {
-                arr[k] = temp[i-l];
+            } else if (j > r) {
+                arr[k] = temp[i - l];
                 i++;
-            } else if(temp[i-l].compareTo(temp[j-l])<=0) {
-                arr[k] = temp[i-l];
+            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
+                arr[k] = temp[i - l];
                 i++;
             } else {
-                arr[k] = temp[j-l];
+                arr[k] = temp[j - l];
                 j++;
             }
         }
     }
 
     public static void main(String[] args) {
-        int n = 1000000;
-       Integer[] arr = ArrayGenerator.generateRandomArray(n,n);
-       SortingHelper.sortTest("MergeSort",arr);
+        int n = 5000000;
+        Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+        Integer[] arr2  =Arrays.copyOfRange(arr,0,arr.length-1);
+        SortingHelper.sortTest("MergeSort", arr);
+        SortingHelper.sortTest("MergeSortBU",arr2);
     }
 }
