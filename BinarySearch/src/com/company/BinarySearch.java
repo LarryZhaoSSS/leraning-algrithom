de2879282df68fcba1830ed9e5b6ee6e93d3bc73
LarryZhao <larryzhao1994@outlook.com>
2021-09-25 21:04:32 +0800

    BinarySearch

--- BinarySearch/src/com/company/BinarySearch.java
+++ BinarySearch/src/com/company/BinarySearch.java
@@ -5,20 +5,45 @@ public class BinarySearch {
     }
 
     public static <E extends Comparable<E>> int search(E[] data, E target) {
-        return search(data, 0, data.length - 1, target);
+        int l = 0;
+        int r = data.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) == 0) {
+                return mid;
+            }
+            if (data[mid].compareTo(target) < 0) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
     }
 
-    private static <E extends Comparable<E>> int search(E[] data, int l, int r, E target) {
+    public static <E extends Comparable<E>> int searchR(E[] data, E target) {
+        return searchR(data, 0, data.length - 1, target);
+    }
+
+    private static <E extends Comparable<E>> int searchR(E[] data, int l, int r, E target) {
         if (l > r) {
             return -1;
         }
-        int mid = l+(r-l)/2;
-        if(data[mid].compareTo(target) == 0) {
+        int mid = l + (r - l) / 2;
+        if (data[mid].compareTo(target) == 0) {
             return mid;
         }
-        if(data[mid].compareTo(target)<0) {
-            return search(data,mid+1,r,target);
+        if (data[mid].compareTo(target) < 0) {
+            return searchR(data, mid + 1, r, target);
         }
-        return search(data,l,mid-1,target);
+        return searchR(data, l, mid - 1, target);
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1,2,3,4,5,6,7,8};
+        int p1 = searchR(arr,3);
+        System.out.println(p1);
+        int p2 = search(arr,3);
+        System.out.println(p2);
     }
 }
