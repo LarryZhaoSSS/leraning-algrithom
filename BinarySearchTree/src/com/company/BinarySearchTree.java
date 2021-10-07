5f524684b1eb8ee54265e9c22dc920b6706db9fd
LarryZhao <larryzhao1994@outlook.com>
2021-10-07 18:33:59 +0800

    BinarySearchTree preOrderNR

---
+++ BinarySearchTree/src/com/company/BinarySearchTree.java
@@ -0,0 +1,153 @@
+package com.company;
+
+import java.util.Stack;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E e) {
+        root = add(root,e);
+    }
+
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+        return node;
+    }
+    public boolean contains(E e) {
+        return contains(root,e);
+    }
+    private boolean contains(Node node,E e) {
+        if(node == null) {
+            return false;
+        }
+        if(e.compareTo(node.e) == 0) {
+            return true;
+        } else if(e.compareTo(node.e)<0) {
+            return contains(node.left,e);
+        } else {
+            return contains(node.right,e);
+        }
+    }
+    public void preOrder() {
+        preOrder(root);
+    }
+    private void preOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    public void inOrder() {
+        inOrder(root);
+    }
+    private void inOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+    public void postOrder() {
+        postOrder(root);
+    }
+    private void postOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+    public void preOrderNR(){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+            if(cur.right!=null) {
+                stack.push(cur.right);
+            }
+            if(cur.left!=null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root,0,res);
+        return res.toString();
+    }
+    private void generateBSTString(Node node,int depth,StringBuilder res) {
+        if(node == null) {
+            res.append(generateDepthString(depth)+"null\n");
+            return;
+        }
+        res.append(generateDepthString(depth) + node.e+"\n");
+        generateBSTString(node.left,depth+1,res);
+        generateBSTString(node.right,depth+1,res);
+    }
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<depth;i++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        bst.preOrder();
+        System.out.println();
+
+        bst.preOrderNR();
+        System.out.println();
+    }
+}
